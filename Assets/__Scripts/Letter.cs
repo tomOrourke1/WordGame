152cd545573c87b222c34502197f5f560d70a7a9
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-01-28 10:40:35 -0500

    initial add of letter script and prefaab

---
+++ Assets/__Scripts/Letter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public TextMesh tMesh;
+    public Renderer tRend;
+    public bool big=false;
+    private char _c;
+    private Renderer rend;
+
+
+    void Awake()
+    {
+        tMesh = GetComponentInChildren<TextMesh>();
+        tRend = tMesh.GetComponent<Renderer>();
+        rend = GetComponent<Renderer>();
+        visible = false;
+    }
+
+    public char c
+    {
+        get{ return(_c);}
+        set {
+            _c= value;
+            tMesh.text = _c.ToString();
+        }
+    }
+    public string str
+    {
+        get{ return _c.ToString();}
+        set{c = value[0];}
+    }
+    public bool visable
+    {
+        get{ return (tRend.enabled);}
+        set{tRend.enabled = value;}
+    }
+    public Color color
+    {
+        get{ return(rend.material.color);}
+        set{rend.material.color = value;}
+    }
+    public Vector3 pos
+    {
+        set {
+            transform.position = value;
+        }
+    }
+}
